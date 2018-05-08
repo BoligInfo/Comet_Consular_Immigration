3ffefefc26cfeabead35cc9499346fb9df62bfbf
DjStonie <valdonay@yahoo.dk>
2018-05-08 09:38:07 +0200

    Initial code

---
+++ comet/src/main/java/dk/comet/CometApplication.java
@@ -0,0 +1,12 @@
+package dk.comet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CometApplication.class, args);
+	}
+}
