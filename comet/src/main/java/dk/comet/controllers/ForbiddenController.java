268bef0b19663bcf2a5d7994a5f9479980a3f31a
Jens Bäckvall <jensbackvall@t-nova.org>
2018-05-09 22:35:16 +0200

    add nationality choice page and forbidden questions

---
+++ comet/src/main/java/dk/comet/controllers/ForbiddenController.java
@@ -0,0 +1,22 @@
+package dk.comet.controllers;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *  @author Jens Bäckvall
+ *
+ */
+@Controller
+public class ForbiddenController {
+
+    @RequestMapping(value={"/the_forbidden.html"}, method = RequestMethod.GET)
+    public ModelAndView forgotPassword(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("the_forbidden");
+        return modelAndView;
+    }
+
+}
